78dc3334fcfbebffea1416d35bb2e5f990708e91
aelarache <aelarache@reseau.free.fr>
2024-10-07 17:07:14 +0200

    add hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+
+
+const Hero = () => {
+  return (
+    <>
+    <section>
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative">
+            {/* Brand infos */}
+            <div>
+                <h1>Gym Gives you the perfect <span>Health</span></h1>
+            </div>
+        </div>
+    </section>
+
+    </>
+  )
+}
+
+export default Hero
