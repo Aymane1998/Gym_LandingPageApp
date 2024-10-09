79c5958894621ff63757b27acfbc70f0badc32df
aelarache <aelarache@reseau.free.fr>
2024-10-09 11:25:06 +0200

    Finishing Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import BgImage from './assets/bg.png'
 
 function App() {
  
-
+const bgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize:'cover',
+  backgroundPosition:'center',
+  backGroundAttachement:'fixed'
+}
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
+    <div className='overflow-hidden'>
+      <div style={bgStyle}>
+        <Navbar/>
+        <Hero/>
+      </div>
+      
     </div>
   );
 }
